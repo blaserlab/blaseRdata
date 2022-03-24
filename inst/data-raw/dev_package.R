3a86c0bd5af1c7547001e73ea8f8d7cf72598e83
blaserlab <bradley.blaser@osumc.edu>
2022-03-23 23:07:36 -0400

    added code for drat

--- inst/data-raw/dev_package.R
+++ inst/data-raw/dev_package.R
@@ -10,6 +10,17 @@
 
 # check()
 document()
-# commit and push
-#install
-renv::install("blaserlab/blaseRdata", library = "/usr/lib/R/site-library")
+
+# add, commit, push
+gert::git_add("*")
+gert::git_commit("version 0.0.0.9008")
+gert::git_push()
+
+# build and insert into repo
+pkg_build <- devtools::build()
+
+drat::insertPackage(file = pkg_build,
+                    repodir = "/home/OSUMC.EDU/blas02/network/X/Labs/Blaser/share/data/R/drat/",
+                    action = "archive")
+
+unlink(pkg_build)
