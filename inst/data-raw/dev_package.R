c700901190567a2339b892f73c4c6e6937708bbb
blaserlab <bradley.blaser@osumc.edu>
2022-03-25 17:19:24 -0400

    version 0.0.0.9009

--- inst/data-raw/dev_package.R
+++ inst/data-raw/dev_package.R
@@ -9,18 +9,12 @@
 # use_data_raw()
 
 # check()
-document()
+devtools::document()
 
 # add, commit, push
 gert::git_add("*")
-gert::git_commit("version 0.0.0.9008")
+gert::git_commit("version 0.0.0.9009")
 gert::git_push()
 
 # build and insert into repo
-pkg_build <- devtools::build()
-
-drat::insertPackage(file = pkg_build,
-                    repodir = "/home/OSUMC.EDU/blas02/network/X/Labs/Blaser/share/data/R/drat/",
-                    action = "archive")
-
-unlink(pkg_build)
+blaseRtemplates::dratify(repo_name = "blaserX", repo_dir = "~/network/X/Labs/Blaser/share/data/R/drat")
